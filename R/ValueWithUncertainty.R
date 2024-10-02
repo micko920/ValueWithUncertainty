d3c281af823afd412f6ebc2903df72eab2ec9d0d
Michael <github@ftt6283.org>
2024-10-02 21:25:34 +0000

    fix for vector ctro

--- R/ValueWithUncertainty.R
+++ R/ValueWithUncertainty.R
@@ -13,23 +13,30 @@
 
 #' ValueWithUncertainty(1) # returns object
 #' @export
-ValueWithUncertainty <- function(LowerCI, Value, UpperCI, model = as.numeric, fixed = TRUE, name = "default") {
-  if (!is.numeric(Value)) stop("Value must be numeric", call. = FALSE)
-  if (!is.numeric(LowerCI)) stop("LowerCI must be numeric", call. = FALSE)
-  if (!is.numeric(UpperCI)) stop("UpperCI must be numeric", call. = FALSE)
-  if (any(LowerCI > Value)) stop("LowerCI must be lower than Value", call. = FALSE)
-  if (any(UpperCI < Value)) stop("UpperCI must be higher than Value", call. = FALSE)
-  v <- as.numeric(Value)
-  if (inherits(Value, "ValueWithUncertainty")) {
-    v <- attr(Value, "value")
-  }
-  new_obj <- structure(v,
-    value = v,
-    lci = LowerCI, uci = UpperCI, model = model,
-    fixed = fixed, symbol = name, class = "ValueWithUncertainty"
-  )
-  if (length(new_obj) == 1) return(simplify2array(new_obj)[[1]])
-  return(simplify2array(new_obj))
+ValueWithUncertainty <- function(LowerCI, Value, UpperCI, model = function(v, n = 1) { attr(v,"value") }, fixed = TRUE) {
+  check_v <- Value
+  if (any(ifelse(class(Value) == "ValueWithUncertainty", TRUE,FALSE))) check_v <- ValueWithUncertaintyValue(Value)
+  if (any(ifelse(!is.numeric(check_v),TRUE,FALSE))) stop("Value must be numeric", call. = FALSE)
+  if (any(ifelse(!is.numeric(LowerCI),TRUE,FALSE))) stop("LowerCI must be numeric", call. = FALSE)
+  if (any(ifelse(!is.numeric(UpperCI),TRUE,FALSE))) stop("UpperCI must be numeric", call. = FALSE)
+  if (any(ifelse(LowerCI > check_v,TRUE,FALSE))) stop("LowerCI must be lower than Value", call. = FALSE)
+  if (any(ifelse(UpperCI < check_v,TRUE,FALSE))) stop("UpperCI must be higher than Value", call. = FALSE)
+  new_obj <- apply(
+      cbind(LowerCI,Value, UpperCI),1,
+      function(p) {
+        m <- check_ctor(p[[1]],p[[2]],p[[3]],model)
+        if (inherits(p[[2]], "ValueWithUncertainty")) {
+          v <- attr(p[[2]], "value")
+        } else v <- p[[2]]
+        return(list(structure(as.numeric(p[[2]]),
+                              value = v,
+                              lci = p[[1]], uci = p[[3]], model = m,
+                              fixed = fixed, class = "ValueWithUncertainty"
+        )))
+      }
+    )
+    if (length(new_obj) == 1) return(simplify2array(new_obj)[[1]])
+    return(simplify2array(new_obj))
 }
 
 #' @export
@@ -37,6 +44,12 @@ ValueWithUncertaintyValue <- function(x) UseMethod("ValueWithUncertaintyValue")
 
 #' @export
 ValueWithUncertaintyValue.ValueWithUncertainty <- function(x) attr(x, "value")
+
+
+#' @export
+ValueWithUncertaintyValue.list <- function(x, ...) {
+  return (sapply(x,ValueWithUncertaintyValue,simplify = FALSE,...))
+}
 
 #' @export
 ValueWithUncertaintyName <- function(x) UseMethod("ValueWithUncertaintyName")
@@ -46,10 +59,21 @@ ValueWithUncertaintyName.ValueWithUncertainty <- function(x) attr(x, "symbol")
 
 
 #' @export
+ValueWithUncertaintyName.list <- function(x, ...) {
+  return (sapply(x,ValueWithUncertaintyName,simplify = FALSE,...))
+}
+
+#' @export
 ValueWithUncertaintyMax <- function(x) UseMethod("ValueWithUncertaintyMax")
 
 #' @export
 ValueWithUncertaintyMax.ValueWithUncertainty <- function(x) attr(x, "uci")
+
+
+#' @export
+ValueWithUncertaintyMax.list <- function(x, ...) {
+  return (sapply(x,ValueWithUncertaintyMax,simplify = FALSE,...))
+}
 
 #' @export
 ValueWithUncertaintyMin <- function(x) UseMethod("ValueWithUncertaintyMin")
@@ -57,11 +81,23 @@ ValueWithUncertaintyMin <- function(x) UseMethod("ValueWithUncertaintyMin")
 #' @export
 ValueWithUncertaintyMin.ValueWithUncertainty <- function(x) attr(x, "lci")
 
+
+#' @export
+ValueWithUncertaintyMin.list <- function(x, ...) {
+  return (sapply(x,ValueWithUncertaintyMin,simplify = FALSE,...))
+}
+
 #' @export
 ValueWithUncertaintyModel <- function(x) UseMethod("ValueWithUncertaintyModel")
 
 #' @export
 ValueWithUncertaintyModel.ValueWithUncertainty <- function(x) attr(x, "model")
+
+
+#' @export
+ValueWithUncertaintyModel.list <- function(x, ...) {
+  return (sapply(x,ValueWithUncertaintyModel,simplify = FALSE,...))
+}
 
 #' @export
 ValueWithUncertaintySampled <- function(x) UseMethod("ValueWithUncertaintySampled")
